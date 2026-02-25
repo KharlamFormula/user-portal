e49a0da61400212b5def7e673996f33b83aa2c7b
KharlamFormula <harlamformula@i.ua>
2026-02-25 15:41:17 +0200

    Move answer page into

--- server/server.js
+++ server/server.js
@@ -44,4 +44,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
+app.get('/', (req, res) => {
+  res.send('Backend працює 🚀');
+});
+
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
